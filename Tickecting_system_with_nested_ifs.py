536527727e7203f486003de2e4982f2d78d10182
Azegba Kizito Amandianeze <kizitoamandi96@gmail.com>
2024-09-21 23:05:47 +0100

    updated my file

--- Tickecting_system_with_nested_ifs.py
+++ Tickecting_system_with_nested_ifs.py
@@ -1,14 +1,25 @@
 #Check the heigth of the game player
 height = float(input(" Enter your height :"))
+bill = 0
 if height >= 120:
     print("Welcome to the rollercoaster!")
     age = int(input("Enter your age : "))
     if age < 12:
-        print("You will pay $5 to ride rollercoaster")
+        bill = 5
+        print("Children Ticket is $5")
     elif age <= 18:
-        print("You will pay $7 to play rollercoaster")
+        bill = 7
+        print("Youth Ticket is $7")
     else:
-        print("You will pay $12 to play rollercoaster")
+        bill = 12
+        print("Adult Ticket is $12")
+
+
+    photo_taken = input("Do you want your phot taken: Y/N: ").lower()
+    if photo_taken == "Y":
+        bill +=bill
+
+    print(f"Total bill is {bill}")
 
 else:
     print("You can't play the rollercoaster, you have to grow taller")
